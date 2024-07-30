3d578e220994bec8b8fd72064e1450f35ddcde41
nihadmirza <nihadmirzezade1@gmail.com>
2024-07-30 18:29:25 +0400

    Uptade

--- src/main/java/com/example/repository/LessonRepository.java
+++ src/main/java/com/example/repository/LessonRepository.java
@@ -2,8 +2,7 @@ package com.example.repository;
 
 import com.example.entity.LessonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface LessonRepository extends JpaRepository<LessonEntity, Long> {
 }
--- src/main/java/com/example/repository/TeacherRepository.java
+++ src/main/java/com/example/repository/TeacherRepository.java
@@ -2,8 +2,7 @@ package com.example.repository;
 
 import com.example.entity.TeacherEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface TeacherRepository extends JpaRepository<TeacherEntity, Long> {
 }
